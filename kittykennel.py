cec9e8d1d1bcd82177ca5d820d6c2f1a0dd3ca3c
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-02-03 19:56:51 -0600

    learned list manipulation

---
+++ kittykennel.py
@@ -0,0 +1,9 @@
+cat_names = ["Clawdia", "Jennifur", "Furby"]
+print(cat_names[0:2])
+cat_names.append("Tigi")
+""" del cat_names[0:2]
+print(cat_names) """
+cat_names = cat_names[2:]
+print(cat_names) 
+cat_names.extend(["Hamster", "Dog", "Chicken"])
+print(cat_names) 
---
+++ list.py
@@ -0,0 +1,5 @@
+stock_history = [186.86, 187.33, 179.25]
+print(stock_history)
+print(stock_history[1])
+print(stock_history[-1])
+print(stock_history[1:]) #same as print(stock_history[1:3] even though index 3 does not exist)
